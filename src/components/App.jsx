166a1294f1f14ecddd46930bef8e144f9272df30
Bartosz <boleksy14@gmail.com>
2022-05-31 20:17:37 +0200

    added local storage functions to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import './App.css'
 
 export default class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: ''
    }
 
@@ -37,6 +32,17 @@ export default class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidMount () {
+   const parsedContacts = JSON.parse(localStorage.getItem("contacts"))
+   if (localStorage.getItem("contacts")) {this.setState ({ contacts: [...parsedContacts] })};
+  }
+  
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+    }
+  }
 
   render() {   
     const toLowFilter = this.state.filter.toLowerCase()
